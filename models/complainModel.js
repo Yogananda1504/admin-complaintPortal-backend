328b74b03e59bcdc5459f5e6c4c0f3436ba1cd8d
Yogananda1504 <vynr1504@gmail.com>
2024-10-31 12:29:36 +0530

    Add complain model with required fields and default values

---
+++ models/complainModel.js
@@ -0,0 +1,50 @@
+import { model, Schema } from 'mongoose';
+
+const complainSchema = new Schema({
+    scholarNumber: {
+        type: String,
+        required: [true, "Student ID is required!"],
+    },
+    studentName: {
+        type: String,
+        required: [true, "Student name is required!"],
+        trim: true
+    },
+    hostelNumber: {
+        type: Intl,
+        required: [true, "Hostel number is required!"],
+    },
+    complainType: {
+        type: String,
+        trim: true
+    },
+    complainDescription: {
+        type: String,
+        trim: true,
+    },
+    attachments: {
+        type: [String],
+    },
+    room :{
+        type : String,
+        trim : true
+    },
+    status: {
+        type: String,
+        default: "Pending",
+        enum: ["Pending", "Resolved"],
+    },
+    read: {
+        type:String,
+        default:"Not viewed",
+        enum:["Not viewed","Viewed"]
+    }
+}, {
+    timestamps: true
+});
+
+complainSchema.index({ studentID: 1 });
+
+const Complaints = model('Complaints', complainSchema);
+
+export default Complaints;
